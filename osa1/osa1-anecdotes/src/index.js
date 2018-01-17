c978cf92c53c81e8817a71e90aa04b47326250cb
Juurinen Juho T <juho.juurinen@gmail.com>
2018-01-17 22:04:07 +0200

    exercise 1.13

--- osa1/osa1-anecdotes/src/index.js
+++ osa1/osa1-anecdotes/src/index.js
@@ -1,23 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+class Anecdote {
+  constructor(text) {
+    this.text = text
+    this.votes = 0
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selected: 0
+      selected : 0,
+      anecdotes : props.anecdotes
     }
   }
 
   randomAnecdote = () => {
-    let i = Math.floor(Math.random() * anecdotes.length)
-    this.setState({ selected : i })
+    let rnd = Math.floor(Math.random() * anecdotes.length)
+    this.setState({ selected : rnd })
+  }
+
+  upvote = () => {
+    let anecdotes = this.state.anecdotes
+    anecdotes[this.state.selected].votes++
+    this.setState({ anecdotes : anecdotes })
   }
 
   render() {
     return (
       <div>
-        <h4>{this.props.anecdotes[this.state.selected]}</h4>
+        <Display a={this.state.anecdotes[this.state.selected]} />
+        <Button
+          handleClick={this.upvote}
+          text="Vote"
+        />
         <Button
           handleClick={this.randomAnecdote}
           text="Keep 'em coming!"
@@ -27,6 +45,15 @@ class App extends React.Component {
   }
 }
 
+const Display = ({ a }) => {
+  return (
+    <div>
+      <h4>{a.text}</h4>
+      <p>This anecdote has {a.votes} votes</p>
+    </div>
+  )
+}
+
 const Button = ({ handleClick, text }) => {
   return (
     <input type="button" onClick={handleClick} value={text} />
@@ -34,13 +61,13 @@ const Button = ({ handleClick, text }) => {
 }
 
 const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-  'If you\'re brave enough, everything is a dildo.'
+  new Anecdote("If it hurts, do it more often"),
+  new Anecdote("Adding manpower to a late software project makes it later!"),
+  new Anecdote("The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time."),
+  new Anecdote("Any fool can write code that a computer can understand. Good programmers write code that humans can understand."),
+  new Anecdote("Premature optimization is the root of all evil."),
+  new Anecdote("Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."),
+  new Anecdote("Everything is a dildo if you're brave enough.")
 ]
 
 ReactDOM.render(
