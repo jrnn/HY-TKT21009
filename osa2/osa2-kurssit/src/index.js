08dd6e9c76e167bd94f5b133d06b302fca0a982d
Juurinen Juho T <juho.juurinen@gmail.com>
2018-01-18 13:04:25 +0200

    exercise 2.4

--- osa2/osa2-kurssit/src/index.js
+++ osa2/osa2-kurssit/src/index.js
@@ -2,39 +2,69 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const kurssi = {
-    nimi : 'Half Stack -sovelluskehitys',
-    osat : [
-      {
-        id : 1,
-        nimi : 'Reactin perusteet',
-        tehtavia : 10
-      },
-      {
-        id : 2,
-        nimi : 'Tiedonvälitys propseilla',
-        tehtavia : 7
-      },
-      {
-        id : 3,
-        nimi : 'Komponenttien tila',
-        tehtavia : 14
-      },
-      {
-        id : 4,
-        nimi : 'Komponenttien määrittely moduuleissa',
-        tehtavia : 8
-      },
-      {
-        id : 5,
-        nimi : 'Taulukossa olevan datan renderöinti',
-        tehtavia : 11
-      }
-    ]
-  };
+  const data = [
+    {
+      id : 1,
+      nimi : 'Half Stack -sovelluskehitys',
+      osat : [
+        {
+          id : 1,
+          nimi : 'Reactin perusteet',
+          tehtavia : 10
+        },
+        {
+          id : 2,
+          nimi : 'Tiedonvälitys propseilla',
+          tehtavia : 7
+        },
+        {
+          id : 3,
+          nimi : 'Komponenttien tila',
+          tehtavia : 14
+        },
+        {
+          id : 4,
+          nimi : 'Komponenttien määrittely moduuleissa',
+          tehtavia : 8
+        },
+        {
+          id : 5,
+          nimi : 'Taulukossa olevan datan renderöinti',
+          tehtavia : 11
+        }
+      ]
+    },
+    {
+      id : 2,
+      nimi : 'Paritanssien ihmeellinen maailma',
+      osat : [
+        {
+          id : 1,
+          nimi : 'Valssi',
+          tehtavia : 23
+        },
+        {
+          id : 2,
+          nimi : 'Tango',
+          tehtavia : 17
+        },
+        {
+          id : 3,
+          nimi : 'Jenkka',
+          tehtavia : 18
+        }
+      ]
+    }
+  ]
+
+  const kurssit = () => data.map(kurssi =>
+    <Kurssi key={kurssi.id} kurssi={kurssi} />
+  )
 
   return (
-    <Kurssi kurssi={kurssi} />
+    <div>
+      {kurssit()}
+    </div>
   )
 }
 
