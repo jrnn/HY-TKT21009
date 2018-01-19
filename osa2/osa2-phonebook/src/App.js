989c5621d4a00dd9732f516af6b5dab16740da86
Juurinen Juho T <juho.juurinen@gmail.com>
2018-01-19 19:31:48 +0200

    exercise 2.9

--- osa2/osa2-phonebook/src/App.js
+++ osa2/osa2-phonebook/src/App.js
@@ -6,25 +6,42 @@ class App extends React.Component {
     this.state = {
       persons : [
         {
+          id : 1,
           name : "Spengebeb Squrupunts",
           number : "+1 42 666 1337"
         },
         {
+          id : 2,
           name : "Chuck Norris",
           number : "none of your business"
+        },
+        {
+          id : 3,
+          name : "Spengebeb Chucknorris",
+          number : "+358 40 123 4567"
+        },
+        {
+          id : 4,
+          name : "Spengebeb Nuckchorris",
+          number : "N/A"
         }
       ],
       newName : "",
-      newNumber : ""
+      newNumber : "",
+      filter : ""
     }
   }
 
   listPersons = () => {
+    let s = this.state.filter.toLowerCase()
+
     return (
       <table>
         <tbody>
-          {this.state.persons.map(p =>
-            <tr key={p.name}>
+          {this.state.persons
+            .filter(p => p.name.toLowerCase().includes(s))
+            .map(p =>
+            <tr key={p.id}>
               <td>{p.name}</td>
               <td>{p.number}</td>
             </tr>
@@ -41,13 +58,14 @@ class App extends React.Component {
         this.state.newNumber === "") { return }
 
     if (this.state.persons
-      .map(p => p.name)
-      .includes(this.state.newName)) {
+      .map(p => p.name.toLowerCase())
+      .includes(this.state.newName.toLowerCase())) {
         alert("Voihan nenä! Nimi on jo varattu!")
         return
     }
 
     let person = {
+      id : this.state.persons.length + 1,
       name : this.state.newName,
       number : this.state.newNumber
     }
@@ -69,39 +87,53 @@ class App extends React.Component {
     this.setState({ newNumber : e.target.value })
   }
 
+  handleFilterChange = (e) => {
+    this.setState({ filter : e.target.value })
+  }
+
   render() {
     return (
       <div>
-        <h2>Puhelinluettelo</h2>
-        <form onSubmit={this.addPerson}>
-          <table>
-            <tbody>
-              <tr>
-                <td>Nimi:</td>
-                <td>
-                  <input
-                    value={this.state.newName}
-                    onChange={this.handleNameChange}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>Numero:</td>
-                <td>
-                  <input
-                    value={this.state.newNumber}
-                    onChange={this.handleNumberChange}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <input type="submit" value="Lisää" />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </form>
+        <h1>Puhelinluettelo</h1>
+        <div>
+          Rajaa näytettäviä:
+          <input
+            value={this.state.filter}
+            onChange={this.handleFilterChange}
+          />
+        </div>
+        <h2>Lisää uusi</h2>
+        <div>
+          <form onSubmit={this.addPerson}>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Nimi:</td>
+                  <td>
+                    <input
+                      value={this.state.newName}
+                      onChange={this.handleNameChange}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Numero:</td>
+                  <td>
+                    <input
+                      value={this.state.newNumber}
+                      onChange={this.handleNumberChange}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <input type="submit" value="Lisää" />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </form>
+        </div>
         <h2>Numerot</h2>
         <div>
           {this.listPersons()}
