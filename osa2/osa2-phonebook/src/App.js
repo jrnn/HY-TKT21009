f9071cd60fbed140254cdb6a4c6d4a02b8241d2b
Juurinen Juho T <juho.juurinen@gmail.com>
2018-01-19 19:17:23 +0200

    exercise 2.8

--- osa2/osa2-phonebook/src/App.js
+++ osa2/osa2-phonebook/src/App.js
@@ -5,20 +5,40 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons : [
-        { name : "Spengebeb Squrupunts" }
+        {
+          name : "Spengebeb Squrupunts",
+          number : "+1 42 666 1337"
+        },
+        {
+          name : "Chuck Norris",
+          number : "none of your business"
+        }
       ],
-      newName : ""
+      newName : "",
+      newNumber : ""
     }
   }
 
   listPersons = () => {
     return (
-      this.state.persons.map(p => <li key={p.name}>{p.name}</li>)
+      <table>
+        <tbody>
+          {this.state.persons.map(p =>
+            <tr key={p.name}>
+              <td>{p.name}</td>
+              <td>{p.number}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     )
   }
 
   addPerson = (e) => {
     e.preventDefault()
+
+    if (this.state.newName === "" ||
+        this.state.newNumber === "") { return }
 
     if (this.state.persons
       .map(p => p.name)
@@ -27,12 +47,17 @@ class App extends React.Component {
         return
     }
 
-    let person = { name : this.state.newName }
+    let person = {
+      name : this.state.newName,
+      number : this.state.newNumber
+    }
+
     let persons = this.state.persons.concat(person)
 
     this.setState({
       persons,
-      newName : ""
+      newName : "",
+      newNumber : ""
     })
   }
 
@@ -40,25 +65,47 @@ class App extends React.Component {
     this.setState({ newName : e.target.value })
   }
 
+  handleNumberChange = (e) => {
+    this.setState({ newNumber : e.target.value })
+  }
+
   render() {
     return (
       <div>
         <h2>Puhelinluettelo</h2>
         <form onSubmit={this.addPerson}>
-          <div>
-            Nimi: <input
-              value={this.state.newName}
-              onChange={this.handleNameChange}
-            />
-          </div>
-          <div>
-            <input type="submit" value="Lisää" />
-          </div>
+          <table>
+            <tbody>
+              <tr>
+                <td>Nimi:</td>
+                <td>
+                  <input
+                    value={this.state.newName}
+                    onChange={this.handleNameChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Numero:</td>
+                <td>
+                  <input
+                    value={this.state.newNumber}
+                    onChange={this.handleNumberChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <input type="submit" value="Lisää" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </form>
         <h2>Numerot</h2>
-        <ul>
+        <div>
           {this.listPersons()}
-        </ul>
+        </div>
       </div>
     )
   }
