2a168ff9bef0722e3735336397f8d56cc2596efd
Juurinen Juho T <juho.juurinen@gmail.com>
2018-02-24 21:15:57 +0200

    exercise 5.13

--- osa5/osa5-blogs/front/src/component/simple_blog.test.js
+++ osa5/osa5-blogs/front/src/component/simple_blog.test.js
@@ -3,14 +3,24 @@ import { shallow } from "enzyme"
 import SimpleBlog from "./simple_blog"
 
 describe("<SimpleBlog />", () => {
-  it("renders title, author and no. of likes", () => {
-    let blog = {
+  let blog
+  let blogComponent
+  let mockHandler
+
+  beforeEach(() => {
+    blog = {
       title : "Everything there is to know about boats",
       author : "Boaty McBoatface",
       likes : 1337
     }
 
-    let blogComponent = shallow(<SimpleBlog blog={blog} />)
+    mockHandler = jest.fn()
+    blogComponent = shallow(
+      <SimpleBlog blog={blog} onClick={mockHandler} />
+    )
+  })
+
+  it("renders title, author and no. of likes", () => {
     let contentDiv = blogComponent.find(".content")
     let likesDiv = blogComponent.find(".likes")
 
@@ -18,5 +28,15 @@ describe("<SimpleBlog />", () => {
       .toContain(blog.title, blog.author)
     expect(likesDiv.text())
       .toContain(blog.likes)
+  })
+
+  it("calls event handler twice when like-button is clicked twice", () => {
+    let likeButton = blogComponent.find("button")
+
+    likeButton.simulate("click")
+    expect(mockHandler.mock.calls.length).toBe(1)
+
+    likeButton.simulate("click")
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })
