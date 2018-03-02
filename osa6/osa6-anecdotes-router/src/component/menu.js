dda7e589ab9cc38c38194eb07897ede9aae72c9b
Juurinen Juho T <juho.juurinen@gmail.com>
2018-03-03 00:15:29 +0200

    exercise 6.20

--- osa6/osa6-anecdotes-router/src/component/menu.js
+++ osa6/osa6-anecdotes-router/src/component/menu.js
@@ -1,11 +1,42 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+
+const styleMenu = {
+  background : "#85909b",
+  padding : 10
+}
+
+const styleLink = {
+  color : "white",
+  padding : 10
+}
+
+const linkSelected = {
+  background : "#212c37",
+  color : "white",
+  padding : 10  
+}
 
 const Menu = () => (
-  <div>
-    <Link to="/">anecdotes</Link>&nbsp;|&nbsp;
-    <Link to="/create">create new</Link>&nbsp;|&nbsp;
-    <Link to="/about">about</Link>
+  <div style={styleMenu}>
+    <NavLink
+      exact to="/"
+      style={styleLink}
+      activeStyle={linkSelected}>
+      anecdotes
+    </NavLink>
+    <NavLink
+      exact to="/create"
+      style={styleLink}
+      activeStyle={linkSelected}>
+      create new
+    </NavLink>
+    <NavLink
+      exact to="/about"
+      style={styleLink}
+      activeStyle={linkSelected}>
+      about
+    </NavLink>
   </div>
 )
 
